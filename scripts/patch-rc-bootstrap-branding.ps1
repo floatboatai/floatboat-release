6d6cab8671b8df8ee19190f2a35057111205c07a
振宇 <zhenyulius@163.com>
2026-06-08 15:17:28 +0800

    test: use custom RC bootstrap page and curl downloader

--- scripts/patch-rc-bootstrap-branding.ps1
+++ scripts/patch-rc-bootstrap-branding.ps1
@@ -108,42 +108,52 @@ if (-not $source.Contains('!define MUI_PAGE_CUSTOMFUNCTION_SHOW BootstrapInstFil
   )
 }
 
-if (-not $source.Contains("Var ProductCarouselDialogHandle")) {
+if (-not $source.Contains("Var BootstrapPageDialogHandle")) {
   $source = $source.Replace(
     "Var DownloadPollEmptyTicks",
-    "Var DownloadPollEmptyTicks${newline}Var ProductCarouselDialogHandle${newline}Var ProductCarouselHandle${newline}Var ProductCarouselBitmap1${newline}Var ProductCarouselBitmap2${newline}Var ProductCarouselBitmap3${newline}Var ProductCarouselFrame${newline}Var ProductCarouselTick"
+    "Var DownloadPollEmptyTicks${newline}Var BootstrapPageDialogHandle${newline}Var BootstrapImageHandle${newline}Var BootstrapTitleHandle${newline}Var BootstrapStatusHandle${newline}Var BootstrapMetaHandle${newline}Var BootstrapPercentHandle${newline}Var ProductCarouselBitmap1${newline}Var ProductCarouselBitmap2${newline}Var ProductCarouselBitmap3${newline}Var ProductCarouselFrame${newline}Var ProductCarouselTick"
   )
 }
 
 if (-not $source.Contains("Function BootstrapInstFilesShow")) {
   $functionBlock = Convert-Newlines -Newline $newline -Content @'
 
-!define FLOATBOAT_PRODUCT_PANEL_STYLE 0x5000000E
+!define FLOATBOAT_CHILD_VISIBLE 0x50000000
+!define FLOATBOAT_STATIC_BITMAP_STYLE 0x5000000E
+!define FLOATBOAT_STATIC_TEXT_STYLE 0x50000000
+!define FLOATBOAT_PROGRESS_STYLE 0x50000000
 !define FLOATBOAT_IMAGE_BITMAP 0
 !define FLOATBOAT_LR_LOADFROMFILE 0x00000010
 !define FLOATBOAT_STM_SETIMAGE 0x0172
+!define FLOATBOAT_SW_HIDE 0
 
-Function BootstrapFindInstFilesControls
-  FindWindow $ProductCarouselDialogHandle "#32770" "" $HWNDPARENT
-  ${If} $ProductCarouselDialogHandle != 0
-    GetDlgItem $ProgressBarHandle $ProductCarouselDialogHandle 1004
+!macro BootstrapHideControl ControlId
+  GetDlgItem $0 $BootstrapPageDialogHandle ${ControlId}
+  ${If} $0 != 0
+    System::Call 'USER32::ShowWindow(p $0, i ${FLOATBOAT_SW_HIDE})'
+  ${EndIf}
+!macroend
+
+Function BootstrapFindInstFilesDialog
+  FindWindow $BootstrapPageDialogHandle "#32770" "" $HWNDPARENT
+  ${If} $BootstrapPageDialogHandle == 0
+    StrCpy $BootstrapPageDialogHandle $HWNDPARENT
   ${EndIf}
 FunctionEnd
 
-Function BootstrapWaitForInstFilesControls
+Function BootstrapWaitForInstFilesDialog
   StrCpy $R8 0
 
-  _bootstrapInstFilesControlWaitLoop:
-    Call BootstrapFindInstFilesControls
-    ${If} $ProductCarouselDialogHandle != 0
-    ${AndIf} $ProgressBarHandle != 0
+  _bootstrapInstFilesDialogWaitLoop:
+    Call BootstrapFindInstFilesDialog
+    ${If} $BootstrapPageDialogHandle != 0
       Return
     ${EndIf}
 
     IntOp $R8 $R8 + 1
     ${If} $R8 < 20
       Sleep 50
-      Goto _bootstrapInstFilesControlWaitLoop
+      Goto _bootstrapInstFilesDialogWaitLoop
     ${EndIf}
 FunctionEnd
 
@@ -153,15 +163,40 @@ Function BootstrapInstFilesShow
   File /oname=$PLUGINSDIR\bootstrap-carousel-2.bmp "bootstrap-carousel-2.bmp"
   File /oname=$PLUGINSDIR\bootstrap-carousel-3.bmp "bootstrap-carousel-3.bmp"
 
-  Call BootstrapWaitForInstFilesControls
-  ${If} $ProductCarouselDialogHandle == 0
+  Call BootstrapWaitForInstFilesDialog
+  ${If} $BootstrapPageDialogHandle == 0
     Return
   ${EndIf}
 
-  System::Call 'USER32::CreateWindowExW(i 0, w "STATIC", w "", i ${FLOATBOAT_PRODUCT_PANEL_STYLE}, i 18, i 112, i 438, i 160, p $ProductCarouselDialogHandle, p 0, p 0, p 0) p.r0'
-  StrCpy $ProductCarouselHandle $0
-  ${If} $ProductCarouselHandle == 0
+  !insertmacro BootstrapHideControl 1004
+  !insertmacro BootstrapHideControl 1006
+  !insertmacro BootstrapHideControl 1016
+  !insertmacro BootstrapHideControl 1027
+  !insertmacro BootstrapHideControl 1037
+
+  System::Call 'USER32::CreateWindowExW(i 0, w "STATIC", w "Floatboat", i ${FLOATBOAT_STATIC_TEXT_STYLE}, i 18, i 14, i 438, i 22, p $BootstrapPageDialogHandle, p 0, p 0, p 0) p.r0'
+  StrCpy $BootstrapTitleHandle $0
+
+  System::Call 'USER32::CreateWindowExW(i 0, w "STATIC", w "", i ${FLOATBOAT_STATIC_BITMAP_STYLE}, i 18, i 44, i 438, i 160, p $BootstrapPageDialogHandle, p 0, p 0, p 0) p.r0'
+  StrCpy $BootstrapImageHandle $0
+  ${If} $BootstrapImageHandle == 0
     Return
+  ${EndIf}
+
+  System::Call 'USER32::CreateWindowExW(i 0, w "STATIC", w "$(TXT_PREPARING)", i ${FLOATBOAT_STATIC_TEXT_STYLE}, i 18, i 218, i 438, i 22, p $BootstrapPageDialogHandle, p 0, p 0, p 0) p.r0'
+  StrCpy $BootstrapStatusHandle $0
+
+  System::Call 'USER32::CreateWindowExW(i 0, w "STATIC", w "0.00% | $(TXT_DOWNLOADING)", i ${FLOATBOAT_STATIC_TEXT_STYLE}, i 18, i 242, i 344, i 18, p $BootstrapPageDialogHandle, p 0, p 0, p 0) p.r0'
+  StrCpy $BootstrapMetaHandle $0
+
+  System::Call 'USER32::CreateWindowExW(i 0, w "STATIC", w "0.00%", i ${FLOATBOAT_STATIC_TEXT_STYLE}, i 366, i 242, i 90, i 18, p $BootstrapPageDialogHandle, p 0, p 0, p 0) p.r0'
+  StrCpy $BootstrapPercentHandle $0
+
+  System::Call 'USER32::CreateWindowExW(i 0, w "msctls_progress32", w "", i ${FLOATBOAT_PROGRESS_STYLE}, i 18, i 266, i 438, i 14, p $BootstrapPageDialogHandle, p 0, p 0, p 0) p.r0'
+  StrCpy $ProgressBarHandle $0
+  ${If} $ProgressBarHandle != 0
+    SendMessage $ProgressBarHandle ${PBM_SETRANGE32} 0 10000
+    SendMessage $ProgressBarHandle ${PBM_SETPOS} 0 0
   ${EndIf}
 
   System::Call 'USER32::LoadImageW(p 0, w "$PLUGINSDIR\bootstrap-carousel-1.bmp", i ${FLOATBOAT_IMAGE_BITMAP}, i 0, i 0, i ${FLOATBOAT_LR_LOADFROMFILE}) p.r0'
@@ -174,15 +209,43 @@ Function BootstrapInstFilesShow
   StrCpy $ProductCarouselFrame 1
   StrCpy $ProductCarouselTick 0
   ${If} $ProductCarouselBitmap1 != 0
-    SendMessage $ProductCarouselHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap1
+    SendMessage $BootstrapImageHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap1
+  ${EndIf}
+FunctionEnd
+
+Function BootstrapEnsureProgressHandle
+  ${If} $ProgressBarHandle == ""
+    Call BootstrapFindInstFilesDialog
+    GetDlgItem $ProgressBarHandle $BootstrapPageDialogHandle 1004
+  ${EndIf}
+  ${If} $ProgressBarHandle == 0
+    Call BootstrapFindInstFilesDialog
+    GetDlgItem $ProgressBarHandle $BootstrapPageDialogHandle 1004
+  ${EndIf}
+FunctionEnd
+
+Function BootstrapRenderCustomStatus
+  ${If} $BootstrapStatusHandle != ""
+  ${AndIf} $BootstrapStatusHandle != 0
+    System::Call 'USER32::SetWindowTextW(p $BootstrapStatusHandle, w "$DownloadStatusLine")'
+  ${EndIf}
+
+  ${If} $BootstrapMetaHandle != ""
+  ${AndIf} $BootstrapMetaHandle != 0
+    System::Call 'USER32::SetWindowTextW(p $BootstrapMetaHandle, w "$DownloadMetaLine")'
+  ${EndIf}
+
+  ${If} $BootstrapPercentHandle != ""
+  ${AndIf} $BootstrapPercentHandle != 0
+    System::Call 'USER32::SetWindowTextW(p $BootstrapPercentHandle, w "$DownloadProgressText")'
   ${EndIf}
 FunctionEnd
 
 Function BootstrapUpdateProductCarousel
-  ${If} $ProductCarouselHandle == ""
+  ${If} $BootstrapImageHandle == ""
     Return
   ${EndIf}
-  ${If} $ProductCarouselHandle == 0
+  ${If} $BootstrapImageHandle == 0
     Return
   ${EndIf}
 
@@ -195,26 +258,50 @@ Function BootstrapUpdateProductCarousel
   ${If} $ProductCarouselFrame == 1
     StrCpy $ProductCarouselFrame 2
     ${If} $ProductCarouselBitmap2 != 0
-      SendMessage $ProductCarouselHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap2
+      SendMessage $BootstrapImageHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap2
     ${EndIf}
   ${ElseIf} $ProductCarouselFrame == 2
     StrCpy $ProductCarouselFrame 3
     ${If} $ProductCarouselBitmap3 != 0
-      SendMessage $ProductCarouselHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap3
+      SendMessage $BootstrapImageHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap3
     ${EndIf}
   ${Else}
     StrCpy $ProductCarouselFrame 1
     ${If} $ProductCarouselBitmap1 != 0
-      SendMessage $ProductCarouselHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap1
+      SendMessage $BootstrapImageHandle ${FLOATBOAT_STM_SETIMAGE} ${FLOATBOAT_IMAGE_BITMAP} $ProductCarouselBitmap1
     ${EndIf}
   ${EndIf}
 FunctionEnd
 
 Function BootstrapDestroyProductCarousel
-  ${If} $ProductCarouselHandle != ""
-  ${AndIf} $ProductCarouselHandle != 0
-    System::Call 'USER32::DestroyWindow(p $ProductCarouselHandle)'
-    StrCpy $ProductCarouselHandle ""
+  ${If} $BootstrapImageHandle != ""
+  ${AndIf} $BootstrapImageHandle != 0
+    System::Call 'USER32::DestroyWindow(p $BootstrapImageHandle)'
+    StrCpy $BootstrapImageHandle ""
+  ${EndIf}
+
+  ${If} $BootstrapTitleHandle != ""
+  ${AndIf} $BootstrapTitleHandle != 0
+    System::Call 'USER32::DestroyWindow(p $BootstrapTitleHandle)'
+    StrCpy $BootstrapTitleHandle ""
+  ${EndIf}
+
+  ${If} $BootstrapStatusHandle != ""
+  ${AndIf} $BootstrapStatusHandle != 0
+    System::Call 'USER32::DestroyWindow(p $BootstrapStatusHandle)'
+    StrCpy $BootstrapStatusHandle ""
+  ${EndIf}
+
+  ${If} $BootstrapMetaHandle != ""
+  ${AndIf} $BootstrapMetaHandle != 0
+    System::Call 'USER32::DestroyWindow(p $BootstrapMetaHandle)'
+    StrCpy $BootstrapMetaHandle ""
+  ${EndIf}
+
+  ${If} $BootstrapPercentHandle != ""
+  ${AndIf} $BootstrapPercentHandle != 0
+    System::Call 'USER32::DestroyWindow(p $BootstrapPercentHandle)'
+    StrCpy $BootstrapPercentHandle ""
   ${EndIf}
 
   ${If} $ProductCarouselBitmap1 != ""
@@ -253,7 +340,14 @@ if (-not $source.Contains("Call BootstrapDestroyProductCarousel${newline}${newli
 if ($source.Contains("GetDlgItem `$ProgressBarHandle `$HWNDPARENT 1004")) {
   $source = $source.Replace(
     "GetDlgItem `$ProgressBarHandle `$HWNDPARENT 1004",
-    "Call BootstrapFindInstFilesControls"
+    "Call BootstrapEnsureProgressHandle"
+  )
+}
+
+if (-not $source.Contains('Call BootstrapRenderCustomStatus')) {
+  $source = $source.Replace(
+    "    StrCpy `$DownloadLastRenderedLine `$0${newline}  `${EndIf}${newline}!macroend",
+    "    StrCpy `$DownloadLastRenderedLine `$0${newline}  `${EndIf}${newline}  Call BootstrapRenderCustomStatus${newline}!macroend"
   )
 }
 
@@ -301,8 +395,10 @@ $requiredSnippets = @(
   '!define MUI_WELCOMEFINISHPAGE_BITMAP "bootstrap-welcome-product.bmp"',
   '!define MUI_PAGE_CUSTOMFUNCTION_SHOW BootstrapInstFilesShow',
   'LangString TXT_READY_TO_LAUNCH',
+  'Var BootstrapPageDialogHandle',
   'Function BootstrapInstFilesShow',
-  'Call BootstrapFindInstFilesControls',
+  'Call BootstrapEnsureProgressHandle',
+  'Call BootstrapRenderCustomStatus',
   'Call BootstrapUpdateProductCarousel',
   'DetailPrint "$(TXT_READY_TO_LAUNCH)"',
   '$DownloadResult"'
@@ -367,64 +463,142 @@ Patch-AtomicWriter `
   -WriteBom $true
 
 $downloadSource = Read-TextFile -Path $downloadScriptPath
-if (-not $downloadSource.Contains('$responseAsyncResult = $request.BeginGetResponse($null, $null)')) {
+$downloadSource = $downloadSource.Replace('$UnknownSizeStepPercent = 0.41', '$UnknownSizeStepPercent = 0.08')
+if (-not $downloadSource.Contains('$curlExe = Join-Path $env:SystemRoot')) {
   $downloadNewline = if ($downloadSource.Contains("`r`n")) { "`r`n" } else { "`n" }
-  $oldConnectBlock = Convert-Newlines -Newline $downloadNewline -Content @'
-  $request = [System.Net.HttpWebRequest]::Create($Url)
-  $request.Method = 'GET'
-  $request.Timeout = 30000
-  $request.ReadWriteTimeout = 30000
-  $response = $request.GetResponse()
-  $totalBytes = [double]$response.ContentLength
-  $inputStream = $response.GetResponseStream()
-'@
+  $newDownloadBlock = Convert-Newlines -Newline $downloadNewline -Content @'
+  $outDir = Split-Path -Parent $OutFile
+  if ($outDir -and -not (Test-Path -LiteralPath $outDir)) {
+    New-Item -ItemType Directory -Path $outDir -Force | Out-Null
+  }
 
-  $newConnectBlock = Convert-Newlines -Newline $downloadNewline -Content @'
-  $request = [System.Net.HttpWebRequest]::Create($Url)
-  $request.Method = 'GET'
-  $request.Timeout = $StallTimeoutMs
-  $request.ReadWriteTimeout = $StallTimeoutMs
-  $request.AllowAutoRedirect = $true
-  $request.UserAgent = 'FloatboatBootstrapInstaller/1.0'
-  $request.KeepAlive = $false
+  $curlExe = Join-Path $env:SystemRoot 'System32\curl.exe'
+  if (-not (Test-Path -LiteralPath $curlExe)) {
+    $curlCommand = Get-Command curl.exe -ErrorAction SilentlyContinue
+    if ($null -eq $curlCommand) {
+      throw (Get-Text -English 'Windows curl.exe is not available on this system.' -Chinese '当前系统找不到 Windows curl.exe。')
+    }
 
-  $response = $null
-  $connectStopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-  $connectDisplayPercent = 0.0
-  $responseAsyncResult = $request.BeginGetResponse($null, $null)
-  try {
-    while (-not $responseAsyncResult.AsyncWaitHandle.WaitOne($ProgressWriteIntervalMs)) {
-      $elapsedMs = [double]$connectStopwatch.ElapsedMilliseconds
-      if ($elapsedMs -ge $StallTimeoutMs) {
-        $request.Abort()
-        throw (Get-Text -English 'The installer download could not connect to the server in time. Please check your network and try again.' -Chinese '安装包下载连接服务器超时，请检查网络后重试。')
+    $curlExe = $curlCommand.Source
+  }
+
+  function Join-ProcessArguments([string[]]$Arguments) {
+    $quoted = @()
+    foreach ($argument in $Arguments) {
+      $value = [string]$argument
+      if ($value.Length -eq 0 -or $value -match '[\s"]') {
+        $value = '"' + ($value -replace '"', '\"') + '"'
       }
 
-      $connectDisplayPercent = [Math]::Min(8.0, $connectDisplayPercent + 0.04)
-      $elapsedSeconds = [Math]::Max(1.0, $elapsedMs / 1000.0)
-      Write-ProgressSnapshot `
-        -DisplayPercent $connectDisplayPercent `
-        -StatusLine (Get-Text -English 'Connecting to the Floatboat download server' -Chinese '正在连接 Floatboat 下载服务器') `
-        -MetaLine (Get-Text -English ([string]::Format($InvariantCulture, '{0:F2}% | Connecting for {1:F0}s', $connectDisplayPercent, $elapsedSeconds)) -Chinese ([string]::Format($InvariantCulture, '{0:F2}% | 已连接 {1:F0} 秒', $connectDisplayPercent, $elapsedSeconds))) `
-        -State 'CONNECTING'
+      $quoted += $value
     }
 
-    $response = [System.Net.HttpWebResponse]$request.EndGetResponse($responseAsyncResult)
-  } finally {
-    if ($responseAsyncResult.AsyncWaitHandle) {
-      $responseAsyncResult.AsyncWaitHandle.Close()
-    }
+    return ($quoted -join ' ')
   }
 
-  $totalBytes = [double]$response.ContentLength
-  $inputStream = $response.GetResponseStream()
+  $partialFile = "$OutFile.download"
+  Remove-Item -LiteralPath $partialFile -Force -ErrorAction SilentlyContinue
+  Remove-Item -LiteralPath $OutFile -Force -ErrorAction SilentlyContinue
+
+  $totalBytes = 0.0
+  $downloadedBytes = 0.0
+  $actualPercent = 0.0
+  $displayPercent = 0.0
+  $unknownSizePercent = 0.0
+  $smoothedSpeedBytesPerSecond = 0.0
+  $lastSpeedSampleBytes = 0.0
+  $lastSpeedSampleAtMs = 0.0
+  $lastProgressWriteAtMs = -1.0
+  $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+  $lastNetworkActivityAtMs = 0.0
+
+  Write-ProgressSnapshot `
+    -DisplayPercent 0.0 `
+    -StatusLine (Get-Text -English 'Starting the Floatboat installer download' -Chinese '正在启动 Floatboat 安装包下载') `
+    -MetaLine (Get-Text -English '0.00% | Connecting through Windows curl' -Chinese '0.00% | 正在通过 Windows curl 建立连接') `
+    -State 'STARTING'
+
+  $curlArguments = @(
+    '--location',
+    '--fail',
+    '--silent',
+    '--show-error',
+    '--connect-timeout', '20',
+    '--speed-time', '30',
+    '--speed-limit', '1024',
+    '--retry', '2',
+    '--retry-delay', '1',
+    '--output', $partialFile,
+    $Url
+  )
+
+  $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+  $processInfo.FileName = $curlExe
+  $processInfo.Arguments = Join-ProcessArguments -Arguments $curlArguments
+  $processInfo.UseShellExecute = $false
+  $processInfo.CreateNoWindow = $true
+  $processInfo.RedirectStandardError = $true
+
+  $curlProcess = [System.Diagnostics.Process]::Start($processInfo)
+  if ($null -eq $curlProcess) {
+    throw (Get-Text -English 'Unable to start Windows curl.exe for the installer download.' -Chinese '无法启动 Windows curl.exe 下载安装包。')
+  }
+
+  while (-not $curlProcess.HasExited) {
+    if (Test-Path -LiteralPath $partialFile) {
+      $currentBytes = [double](Get-Item -LiteralPath $partialFile).Length
+      if ($currentBytes -gt $downloadedBytes) {
+        $downloadedBytes = $currentBytes
+        $lastNetworkActivityAtMs = [double]$stopwatch.ElapsedMilliseconds
+      }
+    }
+
+    Update-DownloadProgressFrame -ForceWrite $true
+
+    if (($stopwatch.ElapsedMilliseconds - $lastNetworkActivityAtMs) -ge $StallTimeoutMs) {
+      try {
+        $curlProcess.Kill()
+      } catch {
+      }
+
+      throw (Get-Text -English 'The installer download did not receive data in time. Please check your network and try again.' -Chinese '安装包下载长时间没有收到数据，请检查网络后重试。')
+    }
+
+    Start-Sleep -Milliseconds $ProgressWriteIntervalMs
+  }
+
+  $curlError = $curlProcess.StandardError.ReadToEnd()
+  if ($curlProcess.ExitCode -ne 0) {
+    $curlError = ($curlError -replace '\r', ' ' -replace '\n', ' ').Trim()
+    if ([string]::IsNullOrWhiteSpace($curlError)) {
+      $curlError = "curl.exe exit code $($curlProcess.ExitCode)"
+    }
+
+    throw (Get-Text -English "The installer download failed: $curlError" -Chinese "安装包下载失败：$curlError")
+  }
+
+  if (Test-Path -LiteralPath $partialFile) {
+    $downloadedBytes = [double](Get-Item -LiteralPath $partialFile).Length
+  }
+
+  if ($downloadedBytes -le 0) {
+    throw (Get-Text -English 'The installer download completed without data.' -Chinese '安装包下载完成但文件为空。')
+  }
+
+  Move-Item -LiteralPath $partialFile -Destination $OutFile -Force
 '@
 
-  if (-not $downloadSource.Contains($oldConnectBlock)) {
-    throw "Unable to patch connection watchdog in $downloadScriptPath"
+  $downloadPattern = '(?s)  \$request = \[System\.Net\.HttpWebRequest\]::Create\(\$Url\).*?  \$response\.Close\(\)\r?\n'
+  if (-not [regex]::IsMatch($downloadSource, $downloadPattern)) {
+    throw "Unable to patch curl downloader in $downloadScriptPath"
   }
 
-  $downloadSource = $downloadSource.Replace($oldConnectBlock, $newConnectBlock)
+  $downloadSource = [regex]::Replace(
+    $downloadSource,
+    $downloadPattern,
+    [System.Text.RegularExpressions.MatchEvaluator] { param($match) $newDownloadBlock },
+    1
+  )
   Write-Utf8BomFile -Path $downloadScriptPath -Content $downloadSource
 }
 
@@ -489,5 +663,6 @@ Write-Host "  downloader script: $downloadScriptPath"
 Write-Host "  setup progress script: $setupProgressScriptPath"
 Write-Host "  welcome image:    $targetWelcomeAssetPath"
 Write-Host "  carousel images:  bootstrap-carousel-1.bmp, bootstrap-carousel-2.bmp, bootstrap-carousel-3.bmp"
-Write-Host "  connection guard: enabled"
+Write-Host "  custom page:      enabled"
+Write-Host "  downloader:       Windows curl.exe with active file-size polling"
 Write-Host "  launch delay:     ${LaunchDelayMs}ms"
